37eb1ddb87e4bcc63d3cd2b45dec655d0fc3f577
caothihongdieu12p <dieucao060620@gmail.com>
2025-02-26 09:16:27 +0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function login()
+{ 
+  let a = username.value
+  let b = password.value
+  if (a == "abc" && b == "123")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block";
+  } 
+else document.write("không hợp lệ")
+}
